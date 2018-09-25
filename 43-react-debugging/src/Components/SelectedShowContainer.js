5469f16bf365593d2134023d9b72ed057ca4b0e2
MinimumViablePerson <nicolas.marcora@yahoo.co.uk>
2018-09-25 17:10:00 +0100

    43-react-debugging

---
+++ 43-react-debugging/src/Components/SelectedShowContainer.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import Episode from './Episode';
+
+class SelectedShowContainer extends Component {
+
+  state = {
+    selectedSeason: 1,
+  }
+
+  mapSeasons = () => {
+    if (this.props.allEpisodes) {
+      let seasons = this.props.allEpisodes.map((e)=> e.season).unique()
+      return seasons.map((s) => {
+        return (<option value={s} key={s}>Season {s}</option>)
+      });
+    }
+  }
+
+  mapEpisodes = () => {
+    return this.props.allEpisodes.map((e) => {
+      if (e.season === this.state.selectedSeason){
+        return (<Episode eachEpisode={e} key={e.id}/>)
+      }
+    })
+  }
+
+  handleSelectionChange = (e) => {
+    this.setState({ selectedSeason: parseInt(e.target.value) })
+  }
+
+
+  render() {
+    const { selectedShow } = this.props
+
+    return (
+      <div style={{position: "static"}}>
+        <h2>{selectedShow.name}</h2>
+        <img src={selectedShow.image.medium} alt=""/>
+        <p dangerouslySetInnerHTML={{__html: selectedShow.summary}}></p>
+        <p>Premiered: {selectedShow.premiered}</p>
+        <p>Status: {selectedShow.status}</p>
+        <p>Average Rating: {selectedShow.rating.average}</p>
+        <select style={{display: 'block'}} onChange={this.handleSelectionChange}>
+          {this.mapSeasons()}
+        </select>
+        {this.mapEpisodes()}
+      </div>
+    );
+  }
+
+}
+
+export default SelectedShowContainer;
+
+
+Array.prototype.unique = function() {
+  var arr = [];
+  for(var i = 0; i < this.length; i++) {
+    if(!arr.includes(this[i])) {
+        arr.push(this[i]);
+    }
+  }
+  return arr;
+}
