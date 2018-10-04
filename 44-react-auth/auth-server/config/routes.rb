eead0c2685da9edbf12281a9f99d05a49472e6a7
MinimumViablePerson <nicolas.marcora@yahoo.co.uk>
2018-10-04 17:22:21 +0100

    auth

--- 44-react-auth/auth-server/config/routes.rb
+++ 44-react-auth/auth-server/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    namespace 'v1' do
+      resources :users
+      resources :items
+      post '/signin', to: 'users#signin'
+      get '/validate', to: 'users#validate'
+    end
+  end
 end
