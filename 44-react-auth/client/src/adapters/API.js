77fc8c4260e32e87c7f12d96c4db58c0d3d975ca
MinimumViablePerson <nicolas.marcora@yahoo.co.uk>
2018-10-16 10:35:05 +0100

    fixed

---
+++ 44-react-auth/client/src/adapters/API.js
@@ -0,0 +1,38 @@
+class API {
+  static signin (username, password) {
+    return fetch(API.signinURL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    }).then(resp => resp.json())
+  }
+
+  static validate (token) {
+    return fetch(API.validateURL, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token
+      }
+    }).then(resp => resp.json())
+  }
+
+  static getItems() {
+    const token = localStorage.getItem('token')
+    return fetch(API.itemsURL, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token
+      }
+    }).then(resp => resp.json())
+  }
+}
+
+API.baseURL = 'http://localhost:3001/api/v1'
+API.signinURL = API.baseURL + '/signin'
+API.validateURL = API.baseURL + '/validate'
+API.itemsURL = API.baseURL + '/items'
+
+export default API
